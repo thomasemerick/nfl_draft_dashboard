a61ec52a6a56cf2f8676d641d0ddfe88df8f7bc6
thomasemerick <49164536+thomasemerick@users.noreply.github.com>
2026-05-17 02:46:31 -0400

    Update 1_NFL_Draft.py

--- pages/1_NFL_Draft.py
+++ pages/1_NFL_Draft.py
@@ -320,7 +320,7 @@ with tab1:
         )
     st.markdown("")
     st.markdown("""
-Each draft slot is attributed points via the Fitzgerald-Spielberger chart and the consensus mock draft slot comes from Arif Hasan's consensus mock draft board.
+Each draft slot is attributed points via the Fitzgerald-Spielberger chart and the consensus mock draft slot comes from Arif Hasan's consensus mock draft board. The net value of each selection and pick swap is determined relative to these metrics, while the Fitzgerald-Spielberger points are also used to assess value of the projected and actual selection number for each pick.
 
 **Adj Total** — aggregate of two components:
 1. Net F-S points from pick # relative to consensus mock draft position
