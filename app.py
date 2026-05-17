a0f3f33b1ca62e6ab0770ecb565393f4e08941a9
thomasemerick <49164536+thomasemerick@users.noreply.github.com>
2026-05-16 22:58:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Viztas", page_icon="📊", layout="wide")
 
-st.title("📊 datavizte")
+st.title("📊 Viztas")
 st.markdown("### Data science applications by Thomas Emerick")
 st.divider()
 
